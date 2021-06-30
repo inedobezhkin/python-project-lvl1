4c6e1c2a2f145425b281caed05ae44de4be577ec
Ivan Macbook <inedobezkin@gmail.com>
2021-06-30 04:26:14 +0300

    Flake8 added

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python
 
 import prompt
 
+
 def print_welcome():
     print('Welcome to the Brain Games!')
+
 
 def greet_player():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
 
+
 def main():
     print_welcome()
     greet_player()
+
 
 if __name__ == '__main__':
     main()
